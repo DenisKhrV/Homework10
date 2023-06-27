0accb6eaf1ba763ad625a3a866d041789c03731c
DenisKhrV <DenisKhr@yandex.ru>
2023-06-27 17:27:37 +1000

    Урок

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+
+    }
+
+    public static void task1() {
+        String street = "манежная";
+        String streetTrue = street.replace("м", "М");
+        System.out.println(streetTrue);
+
+        String phone = "+79-12 96-661-02";
+        phone = phone.replace("-", "");
+        phone = phone.replace(" ", "");
+        phone = phone.replace("+", "");
+
+        if (phone.length() == 10) {
+            phone = "7" + phone;
+        } else if (phone.length() > 11) {
+            throw new RuntimeException("Слишком длинный номер телефона");
+        } else if (phone.length() < 10) {
+            throw new RuntimeException("Слишком короткий номер телефона");
+        } else if (phone.length() == 11 && phone.charAt(0) != '7') {
+            throw new RuntimeException("Иностранный номер");
+        }
+        System.out.println("phone = " + phone);
     }
 }
